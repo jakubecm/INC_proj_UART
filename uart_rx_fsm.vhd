ece84f1cbfabd5a5739f4a3a70e3b038da9c497f
Milan Jakubec <jakubeclp@gmail.com>
2023-04-19 10:51:27 +0200

    Edited states, added probably ideal outputs

--- uart_rx_fsm.vhd
+++ uart_rx_fsm.vhd
@@ -5,17 +5,19 @@ library ieee;
 use ieee.std_logic_1164.all;
 use ieee.std_logic_unsigned.all;
 
-
-
 entity UART_RX_FSM is
     port(
-       CLK : in std_logic;
-       RST : in std_logic;
-       CNT : in std_logic_vector(4 downto 0); -- na pocitani hodinovych signalu, nejvic potrebuju pocitat do 24, takze 4 downto 0
-       CNTBIT : in std_logic_vector(3 downto 0); -- na pocitani zachycenych bitu, nejvic potrebuju pocitat do 8, takze 3 downto 0
-       DIN : in std_logic;
+       -----------Inputy-----------
+       CLK      : in std_logic; -- hodinovy signal
+       RST      : in std_logic; -- reset
+       CNTCLK      : in std_logic_vector(4 downto 0); -- na pocitani hodinovych signalu, nejvic potrebuju pocitat do 24, takze 4 downto 0
+       CNTBIT   : in std_logic_vector(3 downto 0); -- na pocitani zachycenych bitu, nejvic potrebuju pocitat do 8, takze 3 downto 0
+       DIN      : in std_logic; -- datova linka
+       -----------Outputy-----------
        DOUT_VLD : out std_logic
-
+       READ_EN  : out std_logic;
+       CNT_EN   : out std_logic
+       -----------------------------
     );
 end entity;
 
@@ -25,7 +27,11 @@ architecture behavioral of UART_RX_FSM is
     type state_types is (AWAITING_START, STARTED, RECIEVING, AWAITING_STOP, VALID)
     signal state_crrt : state_types := AWAITING_START;
 begin
-
+    -----------------Moorovy vystupy-----------------
+    READ_EN <= '1' when state_crrt = RECIEVING else '0'; -- pokud je v stavu RECIEVING, povol cteni bitu
+    CNT_EN <= '1' when state_crrt = RECIEVING or state_crrt = STARTED else '0'; -- pokud je v stavu RECIEVING nebo STARTED, povol pocitani bitu
+    DOUT_VLD <= '1' when state_crrt = VALID else '0'; -- pokud je v stavu VALID, nastav vystup na 1
+    --------------------------------------------------
     process(CLK) begin
         if rising_edge(CLK) then
             if RST = '1' then
@@ -39,28 +45,27 @@ begin
                     
                     when STARTED =>
 
-                        if CNT = "1000" then -- na osmym cyklu
-                            --Zkontroluj start bit
+                        if CNTCLK = "1000" then -- na osmym clocku hodinoveho signalu
+                            --Zkontroluj start bit--
                             if DIN = '1' then
-                                state_crrt => AWAITING_START;
+                                state_crrt => AWAITING_START; --start bit je spatne, vrat se do stavu AWAITING_START
                             end if;
-                        end if;
 
-                        if CNT = "11000" then
+                        elsif CNTCLK = "11000" then -- na 24. clocku hodinoveho signalu zacni cist data (je to midbit prvniho cteneho bitu)
                             state_crrt <= RECIEVING;
                         end if;
 
                     when RECIEVING =>
-                            if CNTBIT = "1000" then
+                            if CNTBIT = "1000" then -- jestli pocitadlo bitu uz je na osmi, ocekavej stop bit
                                 state_crrt <= AWAITING_STOP;
                             end if;
 
                     when AWAITING_STOP =>
                             if DIN = '1' then
-                                state_crrt <= VALID;
+                                state_crrt <= VALID; -- stop bit je v poradku, prejdi se do stavu VALID
                             end if;
                     
-                    when VALID => state_crrt <= AWAITING_START;
+                    when VALID => state_crrt <= AWAITING_START; -- vrat se do stavu AWAITING_START a cekej na dalsi start bit
                 end case;
             end if;
         end if;
